a10c49e738db2002af62fa86dbf40eb116514197
Wu2ko <98595248+Wu2ko@users.noreply.github.com>
2022-11-03 10:40:49 +0200

    Add files via upload

---
+++ 11_03/11_03/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+
+namespace _11_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Console.WriteLine("************************************");
+            Read();
+            Console.WriteLine("************************************");
+            Search();
+        }
+
+        static void Write()
+        {
+            //irasyti duomenis i faila
+
+            /*string appending = Console.ReadLine();
+
+            if (appending = y OR  )
+            {
+                Console.ReadLine();
+            }
+            
+            */
+
+            string zodziai = Console.ReadLine();
+
+            Console.WriteLine(zodziai);
+
+            //  await File.WriteAllLinesAsync("failas.txt",zodziai);
+
+
+            try
+            {
+                StreamWriter tekstas = new StreamWriter(@"C:\Users\moksleivis\Desktop\Naujas aplankas\failas.txt", true);
+                tekstas.WriteLine(zodziai);
+                tekstas.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void Read()
+        {
+            String line;
+            try
+            {
+
+                StreamReader skaitymas = new StreamReader(@"C:\Users\moksleivis\Desktop\Naujas aplankas\failas.txt");
+                line = skaitymas.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = skaitymas.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                ;
+            }
+        }
+
+        static void Search()
+        {
+            Console.WriteLine("Enter search");
+            string zodis = Console.ReadLine();
+            StreamReader ieskojimas = new StreamReader(@"C:\Users\moksleivis\Desktop\Naujas aplankas\failas.txt");
+            string esamaEilute;
+            bool rastasZodis = false;
+
+            do
+            {
+                esamaEilute = ieskojimas.ReadLine();
+                if (esamaEilute != null)
+                {
+                    rastasZodis = esamaEilute.Contains(zodis);
+                }
+            }
+            while (esamaEilute != null && !rastasZodis);
+            if (rastasZodis == true)
+            {
+                Console.WriteLine("Zodis rastas");
+            }
+            else
+                Console.WriteLine("Zodzio nera");
+        }
+
+
+
+
+        ///methods c#
+        //ctrl k d
+        ///TODOs
+        ///
+        //eilutes i masyva
+
+    }
+}
+
